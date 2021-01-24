e06eb3958d0ffb39e6f2e04e787cacf17c64d683
ayoub-aouad <59152669+ayoub-aouad@users.noreply.github.com>
2021-01-24 23:24:53 +0100

    test

--- hangman/app.js
+++ hangman/app.js
@@ -1,14 +1,30 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 // this is a constructor function 
-const game1 = new hangman('Cat',2)
-puzzleEl.textContent= game1.getPuzzle()
-guessesEl.textContent= game1.getBackStatues()
-
+const game1 = new hangman('Java Script',3)
+puzzleEl.textContent= game1.getPuzzle
+guessesEl.textContent= game1.getBackStatues
 window.addEventListener('keypress', function (e) {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
-    puzzleEl.textContent= game1.getPuzzle()
-    guessesEl.textContent= game1.getBackStatues()
+    puzzleEl.textContent= game1.getPuzzle
+    guessesEl.textContent= game1.getBackStatues
+})
+
+
+// making an http request   
+
+const request = new XMLHttpRequest()
+
+
+request.open('GET','http://puzzle.mead.io/puzzle')
+request.send()
+
+request.addEventListener('readystatechange',(e) => {
+    if (e.target.readyState === 4) {
+        console.log(e.target)
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
 })
 
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,60 +1,60 @@
-const hangman = function (word, remainGusses) {
-    this.word = word.toLowerCase().split('')
-    this.remainGusses = remainGusses
-    this.includeFun = []
-    this.status = 'playing'
-}
-
-hangman.prototype.calculatStatus = function(){
-    const letterUnguessed = this.word.every((letters) => this.includeFun.includes(letters))
-    if (this.remainGusses === 0) {
-        this.status = 'failed'
-    } else if (letterUnguessed) {
-        this.status = 'finished'
-    }else {
+class hangman {
+    constructor (word, remainGusses) {
+        this.word = word.toLowerCase().split('')
+        this.remainGusses = remainGusses
+        this.includeFun = []
         this.status = 'playing'
     }
-}
-
-hangman.prototype.getPuzzle = function () {
-    let puzzle = ''
-    this.word.forEach((letter) => {
-        if (this.includeFun.includes(letter) || letter === ' ') {
-            puzzle += letter
+    calculatStatus () {
+        const letterUnguessed = this.word.every((letters) => this.includeFun.includes(letters) || letters === ' ')
+        if (this.remainGusses === 0) {
+            this.status = 'failed'
+        } else if (letterUnguessed) {
+            this.status = 'finished'
         }else {
-            puzzle += '*'
+            this.status = 'playing'
+        } 
+    }
+    get getPuzzle () {
+        let puzzle = ''
+        this.word.forEach((letter) => {
+            if (this.includeFun.includes(letter) || letter === ' ') {
+                puzzle += letter
+            }else {
+                puzzle += '*'
+            }
+        })
+        return puzzle
+    }
+
+    get getBackStatues () {
+        if (this.status === 'playing') {
+            return `You have ${this.remainGusses} left`
+        }else if (this.status === 'failed') {
+            return `Nice try the word was "${this.word.join('')}"`
+        }else {
+            return `Congradulation you guessed the word` 
         }
-    })
-    return puzzle
-}
-
-hangman.prototype.getBackStatues = function () {
-    if (this.status === 'playing') {
-        return `You have ${this.remainGusses} left`
-    }else if (this.status === 'failed') {
-        return `Nice try the word was "${this.word.join('')}"`
-    }else {
-        return `Congradulation you guessed the word` 
-    }
-}
-
-hangman.prototype.makeGuess = function (guess) {
-    guess = guess.toLowerCase()
-    const isUnique = !this.includeFun.includes(guess)
-    const isBadGuess = !this.word.includes(guess)
-    if(this.status !== 'playing') {
-        return
     }
 
-    if (isUnique) {
-        this.includeFun.push(guess)
+    makeGuess  (guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.includeFun.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
+        if(this.status !== 'playing') {
+            return
+        }
+    
+        if (isUnique) {
+            this.includeFun.push(guess)
+        }
+    
+        if (isUnique && isBadGuess) {
+            this.remainGusses--  
+        }
+    
+        this.calculatStatus()
     }
-
-    if (isUnique && isBadGuess) {
-        this.remainGusses--  
-    }
-
-    this.calculatStatus()
 }
 
 
